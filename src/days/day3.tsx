7c1b02f46a2761e12b616b0ec3ccad014d383ebd
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2023-10-19 11:03:28 +0900

    Day 3 is finished

--- src/days/day3.tsx
+++ src/days/day3.tsx
@@ -1,85 +1,105 @@
-// https://100dayscss.com/days/2/
+// https://100dayscss.com/days/3/
 
 import { cn } from '@helper/utils';
-import * as React from 'react';
-
-const cssVariables = {
-  '--line-height': '8px',
-  '--line-width': '80px',
-  '--line-space': '14px',
-  '--icon-height': 'calc(3*var(--line-height)+2*var(--line-space))',
-};
 
 const keyframes = `
-@keyframes line1-enter {
+@keyframes sun-goes-down {
   0% {
-    transform: translate3d(0, 0, 0) rotate(0deg);
+		background: #F57209;
+    transform: rotate(-70deg);
   }
-  50% {
-    transform: translate3d(0, calc(var(--line-height) + var(--line-space)), 0) rotate(0);
-  }
+	
+	30% {
+		background: #FFEF00;
+		transform: rotate(-28deg);
+	}
+	
+	70% {
+		background: #FFEF00;
+	}
 
   100% {
-    transform: translate3d(0, calc(var(--line-height) + var(--line-space)), 0) rotate(45deg);
+		background: #F57209;
+    transform: rotate(70deg);
   }
 }
 
-@keyframes line2-enter {
+@keyframes sky-turns-black {
   0% {
-    transform: scale(1);
-    opacity: 1;
+    background: #272C34;
   }
+	
+	30% {
+		background: #7DDFFC;
+	}
+	
+	70% {
+		background: #7DDFFC;
+	}
+
   100% {
-    transform: scale(0);
-    opacity: 0;
+    background: #272C34;
   }
 }
 
-@keyframes line3-enter {
+@keyframes pyramide-shading {
   0% {
-    transform: translate3d(0, 0, 0) rotate(0deg);
+    background: #272C34;
   }
-  50% {
-    transform: translate3d(0, calc(0px - var(--line-height) - var(--line-space)), 0) rotate(0);
-  }
+	
+	30% {
+		background: #F4F4F4;
+	}
+	
+	70% {
+		background: #DDDADA;
+	}
+
   100% {
-    transform: translate3d(0, calc(0px - var(--line-height) - var(--line-space)), 0) rotate(135deg);
+    background: #272C34;
   }
 }
 
-@keyframes line1-exit {
-  100% {
-    transform: translate3d(0, 0, 0) rotate(0deg);
-  }
-  50% {
-    transform: translate3d(0, calc(var(--line-height) + var(--line-space)), 0) rotate(0);
-  }
+@keyframes shadow-on-the-floor {
   0% {
-    transform: translate3d(0, calc(var(--line-height) + var(--line-space)), 0) rotate(45deg);
+		transform: scaleY(0);
+	  clip-path: polygon(115px 0%, 231px 0%, 100% 100%);
+  }
+	
+	30% {
+		transform: scaleY(1);
+	  clip-path: polygon(115px 0%, 231px 0%, 80% 100%);
+	}
+	
+	55% {
+		transform: scaleY(.4);
+	}
+
+	75% {
+		transform: scaleY(1);
+	}
+
+  100% {
+		transform: scaleY(0);
+	  clip-path: polygon(115px 0%, 231px 0%, 0% 100%);
   }
 }
 
-
-@keyframes line2-exit {
+@keyframes fading-sand {
   0% {
-    transform: scale(0);
-    opacity: 0;
+    background: #272C34;
   }
-  100% {
-    transform: scale(1);
-    opacity: 1;
-  }
-}
+	
+	30% {
+		background: #F0DE75;
+	}
+	
+	70% {
+		background: #F0DE75;
+	}
 
-@keyframes line3-exit {
-  0% {
-    transform: translate3d(0, calc(0px - var(--line-height) - var(--line-space)), 0) rotate(135deg);
-  }
-  50% {
-    transform: translate3d(0, calc(0px - var(--line-height) - var(--line-space)), 0) rotate(0);
-  }
   100% {
-    transform: translate3d(0, 0, 0) rotate(0deg);
+    background: #272C34;
   }
 }
 `;
@@ -87,7 +107,6 @@ const keyframes = `
 const Day3 = () => {
   return (
     <div
-      style={cssVariables as React.CSSProperties}
       className={cn(
         'flex items-center justify-center',
         'h-[400px] w-[400px]',
@@ -95,7 +114,92 @@ const Day3 = () => {
         'bg-[#272C34]',
         'shadow-[1px_2px_10px_0px_rgba(0,0,0,0.3)]',
       )}
-    ></div>
+    >
+      <style> {keyframes} </style>
+      <div
+        id="circle"
+        className={cn(
+          'relative',
+          'h-[180px] w-[180px]',
+          'rounded-full',
+          'bg-[#272C34]',
+          'overflow-hidden',
+        )}
+      >
+        <div
+          id="sky"
+          className={cn(
+            'absolute z-0',
+            'left-0 right-0 top-0',
+            'h-[124px]',
+            'bg-[#7DDFFC]',
+            'animate-[sky-turns-black_4s_cubic-bezier(.4,0,.49,1)_infinite]',
+          )}
+        />
+        <div
+          id="sun"
+          className={cn(
+            'absolute z-10',
+            'left-[73px] top-[7px]',
+            'h-[34px] w-[34px]',
+            'rounded-full',
+            'bg-[#FFEF00]',
+            'origin-[50%_400%]',
+            'animate-[sun-goes-down_4s_cubic-bezier(.4,0,.49,1)_infinite]',
+          )}
+        />
+        <div
+          id="side-left"
+          style={{
+            clipPath: 'polygon(0% 100%, 100% 100%, 50% 0%)',
+          }}
+          className={cn(
+            'absolute',
+            'left-[35px] top-[67px]',
+            'h-[57px] w-[116px]',
+            'bg-[#F4F4F4]',
+            'animate-[pyramide-shading_4s_cubic-bezier(.4,0,.49,1)_infinite]',
+          )}
+        />
+        <div
+          id="side-right"
+          style={{
+            clipPath: 'polygon(30% 100%, 100% 100%, 0% 0%)',
+          }}
+          className={cn(
+            'absolute',
+            'left-[93px] top-[67px]',
+            'h-[57px] w-[58px]',
+            'bg-[#DDDADA]',
+            'animate-[pyramide-shading_4s_cubic-bezier(.4,0,.49,1)_infinite_reverse]',
+          )}
+        />
+        <div
+          id="shadow"
+          style={{
+            clipPath: 'polygon(115px 0%, 231px 0%, 80% 100%)',
+          }}
+          className={cn(
+            'absolute z-20',
+            'left-[-80px] top-[124px]',
+            'h-[30px] w-[360px]',
+            'bg-[rgba(0,0,0,0.2)]',
+            'origin-[50%_0%]',
+            'animate-[shadow-on-the-floor_4s_cubic-bezier(.4,0,.49,1)_infinite]',
+          )}
+        />
+        <div
+          id="ground"
+          className={cn(
+            'absolute z-10',
+            'bottom-0 left-0 right-0',
+            'h-[56px]',
+            'bg-[#F0DE75]',
+            'animate-[fading-sand_4s_cubic-bezier(.4,0,.49,1)_infinite]',
+          )}
+        />
+      </div>
+    </div>
   );
 };
 
